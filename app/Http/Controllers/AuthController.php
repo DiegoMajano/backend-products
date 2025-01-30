9acc9a1ede64a0724ba73d23b3dae6f887a469f5
DiegoMajano <dmajano302@gmail.com>
2025-01-30 08:29:24 -0600

    adding authcontroller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+    //metood para login
+
+    public function login(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'email'=>'required|email',
+            'password'=> ''
+        ]);
+
+
+        if($validator->fails()){
+            return response()->json([
+                'meesage' => 'Validator error', 
+                'errors'=> $validator->errors()
+            ],400);
+        }
+    }
 }
